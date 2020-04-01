3fa4dee6bc44f7b7d4c0932d901384a98a1128c5
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-04-01 21:07:39 +0300

    use api_check with url parameter

--- api_check.py
+++ api_check.py
@@ -3,22 +3,7 @@ import requests
 import json
 
 
-df = pd.read_csv('data/data.txt',  delimiter=';', encoding='utf8')
-df = df[df['DATA_TYPE'] == 'TRAIN']
-df = df.drop(labels = ["POLICY_IS_RENEWED", 'DATA_TYPE'],axis = 1)
-
-data = df.sample(1)
-data_json = data.to_json(orient='records', indent=2, force_ascii=False)
-
-print('Auto Insurance Churn API')
-print('Requests:')
-print(data_json)
-
-# for local test use
-# url = 'http://127.0.0.1:5000/predict'
-# url = 'https://auto-insurance-churn-api.herokuapp.com/predict'
-
-def check_api(server_url, api):
+def check_api(server_url, api, data_json):
     url = server_url + api
     response = requests.post(url, json=data_json)
 
@@ -27,13 +12,33 @@ def check_api(server_url, api):
     except:
         print(response.text)
 
-server_url = 'http://127.0.0.1:5000/'
 
-print('\nRequests answers:\n')
-print('--- LogisticRegression ---')
-print('Request from {}predict_logreg'.format(server_url))
-check_api(server_url, 'predict_logreg')
-print('\n--- XGboost ---')
-print('Request from {}predict_xgboost'.format(server_url))
-check_api(server_url, 'predict_xgboost')
-print()
+def main(server_url = 'http://127.0.0.1:5000/'):
+
+    df = pd.read_csv('data/data.txt',  delimiter=';', encoding='utf8')
+    df = df[df['DATA_TYPE'] == 'TRAIN']
+    df = df.drop(labels = ["POLICY_IS_RENEWED", 'DATA_TYPE'],axis = 1)
+
+    data = df.sample(1)
+    data_json = data.to_json(orient='records', indent=2, force_ascii=False)
+
+    print('Auto Insurance Churn API')
+    print('Requests:')
+    print(data_json)
+    print('\nRequests answers:\n')
+    print('--- LogisticRegression ---')
+    print('Request from {}predict_logreg'.format(server_url))
+    check_api(server_url, 'predict_logreg', data_json)
+    print('\n--- XGboost ---')
+    print('Request from {}predict_xgboost'.format(server_url))
+    check_api(server_url, 'predict_xgboost', data_json)
+    print()
+
+import sys
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        main(server_url=url)
+    except:
+        print('Incorrect url')
